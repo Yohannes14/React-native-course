b831509cb013edc745ea03e48352d9ea4b37cb89
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-11 14:18:15 +0300

    make content scrollable

--- App.js
+++ App.js
@@ -1,35 +1,41 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-
+import { useState } from "react";
+import {Button, 
+        ScrollView, 
+        StyleSheet,
+        Text,
+        TextInput,
+        View } from "react-native";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] =useState(' ');
-  const [courseGoals, setCourseGoals] =useState([]);
+  const [enteredGoalText, setEnteredGoalText] = useState(" ");
+  const [courseGoals, setCourseGoals] = useState([]);
 
-const  goalInputHandler = (enteredText) =>{
- setEnteredGoalText(enteredText);
-
-};
-const addInputHandler =() =>{
-setCourseGoals(prev => [
-  ...prev, enteredGoalText
-]);
-  
-};
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+  const addInputHandler = () => {
+    setCourseGoals((prev) => [...prev, enteredGoalText]);
+  };
   return (
-    <View style ={styles.appContainer}>
-      <View style ={styles.inputContainer}>
-        <TextInput style ={styles.textInput} placeholder=' Your course goal!'
-        onChangeText={goalInputHandler}
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder=" Your course goal!"
+          onChangeText={goalInputHandler}
         />
-        <Button title='Add Goal!'
-         onPress={addInputHandler}
-        />
+        <Button title="Add Goal!" onPress={addInputHandler} />
       </View>
-      <View style ={styles.goalContainer}>
-        {courseGoals.map((goal, index) => <Text key ={index}>{goal}</Text>)}
+      <View style={styles.goalContainer}>
+        {/* make content scrollable */}
+      <ScrollView alwaysBounceVertical ={false}>
+        {courseGoals.map((goal, index) => (
+          <View key={index} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
       </View>
-    
     </View>
   );
 }
@@ -42,22 +48,29 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
-    borderBottomColor: '#cccccc',
-  
+    borderBottomColor: "#cccccc",
   },
-  textInput:{
+  textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 8,
     padding: 8,
   },
-  goalContainer :{
+  goalContainer: {
     flex: 5,
-  }
- 
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  goalText: {
+    color: "white",
+  },
 });
