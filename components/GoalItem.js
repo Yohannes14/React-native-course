2a2da99bf58e4ac9fab8901d72609bf2d3efb23d
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-11 15:48:48 +0300

    Adding styles and android ripple

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,17 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const GoalItem = ( props) => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
- <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+    <Pressable 
+    android_ripple={{color: '#210644'}} 
+    onPress={props.onDeleteItem.bind(this, props.id)}
+    style ={({pressed}) => pressed && styles.pressedItem}
+    >
+ 
     <Text style={styles.goalText}>{props.text}</Text>
-  </View>
+ 
   </Pressable>
+  </View>
   )
 }
 
@@ -14,12 +20,16 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
       margin: 8,
-      padding: 8,
       borderRadius: 6,
       backgroundColor: "#5e0acc",
     },
+    pressedItem: {
+        opacity: 0.5,
+
+    },
     goalText: {
       color: "white",
+      padding: 8,
     },
   });
   
