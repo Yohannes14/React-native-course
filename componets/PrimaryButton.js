c268a403fa54e385b7aa7f6ff31cc193eacea43f
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-12 10:25:56 +0300

    first commit

---
+++ componets/PrimaryButton.js
@@ -0,0 +1,12 @@
+import { Button, Text, View } from "react-native"
+
+const PrimaryButton = ({childern}) => {
+  return (
+    <View>
+        <Text>{childern}</Text>
+    </View>
+   
+  )
+}
+
+export default PrimaryButton;
