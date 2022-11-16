5af1b960764792e717295e6a3f16eccd4eb4aecf
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-16 10:52:19 +0300

    display items in grid

--- components/CategoryGridTitle.js
+++ components/CategoryGridTitle.js
@@ -1,11 +1,12 @@
-import { Pressable, Text, View } from "react-native";
+import { Platform, Pressable, StyleSheet, Text, View} from "react-native";
 
 const CategoryGridTitle = ({ title, color }) => {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text> {title}</Text>
+    <View style ={styles.gridItem}>
+      <Pressable android_ripple={{color: '#ccc'}}
+      style ={({pressed}) =>[styles.button, pressed ? styles.buttonPressed : null, ]}>
+        <View style ={[styles.innerContainer, {backgroundColor: color}]}> 
+          <Text style ={styles.title}> {title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +14,38 @@ const CategoryGridTitle = ({ title, color }) => {
 };
 
 export default CategoryGridTitle;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4, // for android
+        shadowColor: 'black', // for ios
+        shadowOpacity: 0.25, // for ios
+        shadowOffset: {width: 0, height: 2}, // for ios
+        shadowRadius: 8,  //ios
+        overflow: Platform.OS ==='android' ? 'hidden': 'visible',
+
+    },
+    button: {
+        flex: 1,
+    },
+    buttonPressed:{
+        opacity: 0.5
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+
+    }
+
+})
