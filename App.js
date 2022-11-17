84750340b220aec9010598c5983701121dc0bbd5
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-17 14:14:16 +0300

    Nested navigation

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet} from 'react-native';
+import { StyleSheet} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MealsOverview from './screens/MealsOverview';
 import MealDetailScreen from './screens/MealDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+   return ( <Drawer.Navigator
+           screenOptions={{
+            headerStyle: {backgroundColor: '#351401'},
+           headerTintColor:'white',
+          // sceneContainerStyle: {backgroundColor: '#3f2f25'}
+           }}
+           >
+        <Drawer.Screen name ="Categories" component={CategoriesScreen} options ={{title:'All Categories'}} />
+        <Drawer.Screen name ="Favories" component={FavoritesScreen} />
+
+        </Drawer.Navigator>
+   );
+}
 
 
 export default function App() {
@@ -23,11 +40,12 @@ export default function App() {
      }}>
         {/* <Stack.Screen name="Categories" component={CategoriesScreen} /> */}
         <Stack.Screen  
-            name="AllCategories" 
-            component={CategoriesScreen} 
-            // options ={{
-            //   title: 'All Categories',
-            //  }}
+            name="Drawer" 
+            component={DrawerNavigator} 
+            options ={{
+              //title: 'All Categories',
+              headerShown :false,
+             }}
             
             /> 
  
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,8 @@
+import {Text} from "react-native";
+const FavoritesScreen = () => {
+  return (
+    <Text>FavoritesScreen</Text>
+  )
+}
+
+export default FavoritesScreen
