f28f9e437b981864b83d9b8c7a71723b79292008
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-24 08:45:31 +0300

    creating custom button

--- components/places/ImagePicker.js
+++ components/places/ImagePicker.js
@@ -1,7 +1,14 @@
-import { Alert, Button, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import {launchCameraAsync, PermissionStatus, useCameraPermissions} from 'expo-image-picker';
+import { useState } from "react";
+import { Colors } from "../../constants/Colors";
+import OutLinedButton from "../ui/OutLinedButton";
 
 const ImagePicker = () => {
+
+    const [pickedImage, setPickedImage] = useState();
+
+
     const [cpInf, requestPermission] =useCameraPermissions();
 
     async function verifyPermission(){
@@ -29,17 +36,37 @@ const ImagePicker = () => {
         aspect: [16, 9],
         quality: 0.5,
       });
-      console.log(image);
+
+      setPickedImage(image.uri);
     }
 
+  let imagePreview = <Text> No image taken yet.</Text>
+
+  if(pickedImage){
+    imagePreview =   <Image style ={styles.image} source={{uri: pickedImage}} />
+  }
   return (
     <View>
-        <View>
-
-        </View>
-        <Button title ="Take image" onPress={takeImagehandler} />
+        <View style ={styles.imagePreview}>{imagePreview}</View>
+        <OutLinedButton icon ="camera"  onPress={takeImagehandler}>Take Image</OutLinedButton>
     </View>
   )
-}
+} 
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems:'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+    },
+    image :{
+      width: '100%', 
+      height: '100%',
+    }
+});
